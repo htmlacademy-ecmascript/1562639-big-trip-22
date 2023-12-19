528e3b957d919148b34a420b3fc67a27f6b52e61
JuliannaLazareva <juliannalazareva123@gmail.com>
2023-12-19 19:33:00 +0300

    Добавляет board-presenter

--- src/main.js
+++ src/main.js
@@ -1,15 +1,13 @@
-import SortingView from './view/sorting-view';
 import FilterView from './view/filter-view';
-import PointsListPresenter from './presenter/point-list-presenter';
+import BoardPresenter from './presenter/board-presenter';
 import {render} from './render';
 
 const siteHeaderElement = document.querySelector('.page-header');
 const siteFilterContainer = siteHeaderElement.querySelector('.trip-controls__filters');
 const siteMainElement = document.querySelector('.page-main');
 const siteTripEventsElement = siteMainElement.querySelector('.trip-events');
-const pointListPresenter = new PointsListPresenter({pointListContainer: siteTripEventsElement});
+const boardPresenter = new BoardPresenter({boardContainer: siteTripEventsElement});
 
 render(new FilterView(), siteFilterContainer);
-render(new SortingView(), siteTripEventsElement);
 
-pointListPresenter.init();
+boardPresenter.init();
