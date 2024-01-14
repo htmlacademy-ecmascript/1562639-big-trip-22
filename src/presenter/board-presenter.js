ff63b716d61a78b7a89adf3617a2271241e889b6
JuliannaLazareva <juliannalazareva123@gmail.com>
2024-01-14 10:42:22 +0300

    Выделит показ точек маршрута в функцию

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -1,9 +1,7 @@
 import PointsListView from '../view/points-list-view.js';
 import PointView from '../view/point-view.js';
-import EditPointView from '../view/edit-point-view.js';
 import SortingView from '../view/sorting-view';
 import {render} from '../framework/render.js';
-import { getDefaultPoint } from '../const.js';
 
 export default class BoardPresenter {
   #boardContainer = null;
@@ -28,11 +26,15 @@ export default class BoardPresenter {
 
     render(new SortingView(), this.#boardContainer);
     render(this.#pointListComponent, this.#boardContainer);
-    render(new EditPointView({point: getDefaultPoint(), destinations: this.#destinations, offers: this.#offers}), this.#pointListComponent.element);
-    render(new EditPointView({point: this.#boardPoints[0], destinations: this.#destinations, offers: this.#offers}), this.#pointListComponent.element);
 
-    for (let i = 1; i < this.#boardPoints.length; i++) {
-      render(new PointView({point: this.#boardPoints[i], destinations: this.#destinations, offers: this.#offers}), this.#pointListComponent.element);
+    for (let i = 0; i < this.#boardPoints.length; i++) {
+      this.#renderPoints(this.#boardPoints[i], this.#destinations, this.#offers);
     }
+  }
+
+  #renderPoints(point, destinations, offers) {
+    const pointComponent = new PointView({point, destinations, offers});
+
+    render (pointComponent, this.#pointListComponent.element);
   }
 }
